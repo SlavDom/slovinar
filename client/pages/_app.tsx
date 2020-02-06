c7460ee760852a9ab11ac39c3d0ad577e3d35c00
eakarpov <eakarpov@yandex.ru>
2020-02-06 22:37:36 +0300

    nsl.json

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,27 +1,43 @@
-import App from 'next/app'
-import React from 'react'
-import {grommet, Grommet, extendDefaultTheme} from "grommet";
-import { deepMerge } from "grommet/utils";
-import {Header} from "../layout/header";
+import App from 'next/app';
+import React, {useEffect, useState} from 'react';
+import {grommet, Grommet, extendDefaultTheme} from 'grommet';
+import {deepMerge} from 'grommet/utils';
+import {Header} from '../layout/header';
+import {I18nContext} from '../lib/hooks';
+import {setLanguageAsync} from '../lib/i18n';
 
 extendDefaultTheme(
     deepMerge(grommet, {
         global: {
             colors: {
-                'light-3': "red"
+                'light-3': 'red'
             }
         }
     })
 );
 
-export default class MyApp extends App {
-    render() {
-        const { Component, pageProps } = this.props;
-        return (
+export default function MyApp({Component, pageProps, dictionary, language}) {
+    const [lang, updateLang] = useState(language);
+    const [dict, updateDict] = useState(dictionary);
+
+    // If lang changed, re-request the dictionary
+    useEffect(() => {
+        setLanguageAsync(lang).then((res: any) => {
+            updateDict(res);
+        });
+    }, [lang]);
+    return (
+        <I18nContext.Provider value={dict}>
             <Grommet theme={grommet} plain>
-                <Header />
+                <Header/>
                 <Component {...pageProps} />
             </Grommet>
-        )
-    }
+        </I18nContext.Provider>
+    );
 }
+
+MyApp.getInitialProps = async () => {
+    const language = 'nsl';
+    const dictionary = await setLanguageAsync(language);
+    return { dictionary, language };
+};
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,34 +1,22 @@
-import React, {ChangeEvent, Component} from 'react';
+import React, {ChangeEvent, Component, useState} from 'react';
 import styled from 'styled-components';
 import {Box, TextInput} from 'grommet';
-import {withTranslation} from 'react-i18next';
-import {WithTranslation} from 'next-i18next';
+import {useTranslation} from '../lib/hooks';
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.global.colors.focus};
+  color: ${({theme}) => theme.global.colors.focus};
 `;
 
-class MainPage extends Component<WithTranslation> {
-    public state = {
-        wordName: '',
-    };
+export default function MainPage() {
+    const [wordName, setWordName] = useState('');
+    const prop = useTranslation('test');
 
-    private onChange(e: ChangeEvent<HTMLInputElement>) {
-        this.setState({
-            wordName: e.target.value,
-        });
-    }
-
-    public render() {
-        return <div>
-            <TextInput
-                value={this.state.wordName}
-                onChange={this.onChange}
-            />
-            {this.props.t('test')}
-        </div>
-    }
+    return <div>
+        <TextInput
+            value={wordName}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setWordName(e.target.value)}
+        />
+        <Title>{prop}</Title>
+    </div>;
 }
-
-export default withTranslation('common')(MainPage);
